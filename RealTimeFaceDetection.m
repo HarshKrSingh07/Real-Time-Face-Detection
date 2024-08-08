a8283c120238dd03b457e4402e008f004168c8d3
Harsh Kumar Singh <87273309+HarshKrSingh07@users.noreply.github.com>
2024-08-08 22:03:38 +0530

    Update RealTimeFaceDetection.m

--- RealTimeFaceDetection.m
+++ RealTimeFaceDetection.m
@@ -1,7 +1,7 @@
 %Face Detection
 web = webcam();
 im = snapshot(web);
-dete = vision.CascadeObjectDetector('Nose');
+dete = vision.CascadeObjectDetector();
 dete.MergeThreshold = 150;
 imshow(im);
 
@@ -9,6 +9,6 @@ while true
     im = snapshot(web);
     im2 = rgb2gray(im);
     bbox = step(dete,im2);
-    pic = insertObjectAnnotation(im,'rectangle',bbox,'Nose');
+    pic = insertObjectAnnotation(im,'rectangle',bbox,'Face');
     imshow(pic);
 end
